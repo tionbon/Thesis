068b2ae4c8055ebeeec1f287d888696cf6f7f30d
Tionney Nix <nix.tionney@gmail.com>
2017-03-17 09:27:41 -0400

    merged files with -noRace and -noGender now runs

--- data_retrieval.py
+++ data_retrieval.py
@@ -4,6 +4,32 @@ import csv
 from collections import defaultdict
 
 
+
+def temp():
+	repaired_files = []
+
+	#identifier = dir_
+	#os.makedirs(identifier)
+	
+	for i in range(11):
+		repair_value = i/10.0
+
+		# Access audited file
+		file = open("Data/bonus_indirect.csv", 'rt')
+		repaired_reader = csv.reader(file)
+
+		# Process auditied file
+		repaired_file = open("Data/bonus_indirect.tab", 'w')
+		# convert to tab separated file 
+		repaired_file.write("\t".join(next(repaired_reader)) +"\n")
+		repaired_file.write("d\td\tHighschool Bachelors Masters\t<3 3-6 >6\tlow medium high\td\n")
+		repaired_file.write("\t\t\t\t\tclass\n")
+		for row in repaired_reader:
+			repaired_file.write("\t".join(row)+"\n")
+
+		repaired_file.close()
+		repaired_files.append(repaired_file)
+
 def repaired_data_retrieval(dir_, repaired_dir, repaired_attrib):
 	repaired_files = []
 
@@ -55,7 +81,7 @@ def retrieval_and_merger(orig_file, dir_, repaired_dir, repaired_attrib):
 	
 	repaired_file.write("\t".join(merged)+"\n")
 	repaired_file.write("d\td\td\td\tHighschool Bachelors Masters\tHighschool Bachelors Masters\t<3 3-6 >6\t<3 3-6 >6\tlow medium high\tlow medium high\td\td\n")
-	repaired_file.write("\t\t\t\t\t\t\t\t\t\tignore\tclass\n")
+	repaired_file.write("\t\t\t\t\t\t\t\t\t\tclass\tignore\n")
 
 	# convert to tab separated file, merge and write
 	for orig_row in orig_reader:
@@ -64,6 +90,14 @@ def retrieval_and_merger(orig_file, dir_, repaired_dir, repaired_attrib):
 		merged[::2] = orig_row
 		merged[1::2] = repaired_row
 		repaired_file.write("\t".join(merged)+"\n")
+
+	summary = open("{}/{}_.summary".format(identifier, repaired_attrib), 'w')
+	f = open(repaired_dir + "/summary.txt", 'r')
+	for line in f:
+		if line.startswith('Ranked Features by accuracy:'):
+			ds = line.split(':')[1][1:]
+ 			summary.write(ds)
+ 			summary.close()
 
 	repaired_file.close()
 	repaired_files.append(repaired_file)
@@ -96,15 +130,74 @@ def ricci_repaired_data_retrieval():
 		repaired_file.close()
 		repaired_files.append(repaired_file)
 
+def adult_retrieval_and_merger(orig_file, dir_, repaired_dir, repaired_attrib):
+	repaired_files = []
+
+	identifier = dir_	
+	repair_value = 1.0
+
+	# Access original file
+	f2 = open(orig_file, 'rt')
+	orig_reader = csv.reader(f2)
+
+	# Access audited file
+	f1 = open(repaired_dir + "/{}.audit.test.repaired_{}.data".format(repaired_attrib, repair_value), 'rt')
+	repaired_reader = csv.reader(f1)
+
+	# Open new file for writing
+	repaired_file = open("{}/adult_{}.tab".format(identifier, repaired_attrib), 'w') 
+
+	# Merge Headers
+	orig_header = next(orig_reader)
+	repaired_header = [i+'-no{}'.format(repaired_attrib) for i in next(repaired_reader)]
+	merged = [None]*(len(orig_header)+len(repaired_header))
+	merged[::2] = orig_header
+	merged[1::2] = repaired_header
+	
+	repaired_file.write("\t".join(merged)+"\n")
+	type = "continuous\tPrivate Self-emp-not-inc Self-emp-inc Federal-gov Local-gov State-gov Without-pay Never-worked\t \
+			continuous\tBachelors Some-college 11th HS-grad Prof-school Assoc-acdm Assoc-voc 9th 7th-8th 12th Masters 1st-4th 10th Doctorate 5th-6th Preschool \
+			\tcontinuous\tMarried-civ-spouse Divorced Never-married Separated Widowed Married-spouse-absent Married-AF-spouse \
+			\tTech-support Craft-repair Other-service Sales Exec-managerial Prof-specialty Handlers-cleaners Machine-op-inspct \
+			Adm-clerical Farming-fishing Transport-moving Priv-house-serv Protective-serv Armed-Forces\tWife Own-child Husband \
+			Not-in-family Other-relative Unmarried\tWhite Asian-Pac-Islander Amer-Indian-Eskimo Other Black\tFemale Male\tcontinuous \
+			\tcontinuous\tcontinuous\tUnited-States Cambodia England Puerto-Rico Canada Germany Outlying-US(Guam-USVI-etc) \
+			 India Japan Greece South China Cuba Iran Honduras Philippines Italy Poland Jamaica Vietnam Mexico Portugal \
+			 Ireland France Dominican-Republic Laos Ecuador Taiwan Haiti Columbia Hungary Guatemala Nicaragua Scotland \
+			 Thailand Yugoslavia El-Salvador Trinadad&Tobago Peru Hong Holand-Netherlands\t>50K <=50K\n"
+	col_info = "\t\t\t\t\t\t\t\t\t\t\t\t\t\tclass \n"
+	repaired_file.write(types)
+	repaired_file.write(col_info)
+
+	# convert to tab separated file, merge and write
+	for orig_row in orig_reader:
+		repaired_row = next(repaired_reader)
+		merged = [None]*(len(orig_row)+len(repaired_row))
+		merged[::2] = orig_row
+		merged[1::2] = repaired_row
+		repaired_file.write("\t".join(merged)+"\n")
+
+	summary = open("{}/{}_.summary".format(identifier, repaired_attrib), 'w')
+	f = open(repaired_dir + "/summary.txt", 'r')
+	for line in f:
+		if line.startswith('Ranked Features by accuracy:'):
+			ds = line.split(':')[1][1:]
+ 			summary.write(ds)
+ 			summary.close()
+
+	repaired_file.close()
+	repaired_files.append(repaired_file)
+
 if __name__ == "__main__":
 	#repaired_dir = "./Audit/audits/1487023139.65"
 	#repaired_dir = "./Audit/audits/1487655656.76/"
 	#repaired_dir = "./Audit/audits/1488339786.6" 
 	#repaired_dir = "./Audit/audits/1489282159.55/"
-	repaired_dir = "./Audit/audits/1489282271.25/"
+	#repaired_dir = "./Audit/audits/1489282271.25/"
+	repaired_dir = 
 	attrib = ["Race", "Gender"]
-	orig_file = "./Data/bonus_indirect.csv"
+	orig_file = "./Audit/test_data/adult.csv"
 	for repaired_attrib in attrib:
-		dir_ = "./Data".format(repaired_attrib)
+		dir_ = "./Data"
 		#repaired_data_retrieval(dir_, repaired_dir, repaired_attrib)
 		retrieval_and_merger(orig_file, dir_, repaired_dir, repaired_attrib)
--- make_rules.py
+++ make_rules.py
@@ -2,12 +2,13 @@ import Orange
 import os, sys
 import datetime
 import csv
+import ast
 from collections import defaultdict
 
 path_to_datatables = "/usr/local/lib/python3.5/dist-packages/Orange/datasets/"
 
 
-def make_rules(orig_data, repaired_files, output, beam_width, min_covered_examples, max_rule_length):
+def make_repaired_rules(orig_data, repaired_files, output, beam_width, min_covered_examples, max_rule_length, scores, tag):
 	print("Learning rules from unrepaired file")
 	# format data for classification
 	original_data = Orange.data.Table.from_file(orig_data)
@@ -21,6 +22,9 @@ def make_rules(orig_data, repaired_files, output, beam_width, min_covered_exampl
 	learner_orig.rule_finder.general_validator.min_covered_examples = min_covered_examples
 	# set the maximum number of selectors (conditions) found rules may combine
 	learner_orig.rule_finder.general_validator.max_rule_length = max_rule_length
+	# Calculate discrimination score
+	learner_orig.rule_finder.scores = scores
+
 	# produce rules from unrepaired data
 	classifier_orig = learner_orig(original_data)
 	
@@ -31,6 +35,23 @@ def make_rules(orig_data, repaired_files, output, beam_width, min_covered_exampl
 	for id, rule in enumerate(og_rulelist):
 		rule.ID = id
 		rule_counts[rule.ID] = 0 
+
+	print("Writing original rules to file")
+	# write rules to file
+	with open("Rules/"+output+"-repaired", 'a+') as rules:
+		# Create rules file from repaired data
+		rules.write("******** {} ********\n".format(orig_data))
+		rules.write("Rules\tScore\n")
+		rl = []
+		for rule in classifier_orig.rule_list:
+			rl.append((str(rule), rule.score))
+
+		rl.sort(key=lambda x: x[0])
+		for rule in rl:
+			#rules.write("{}\t{}\n".format(rule, [rule_counts[rule.ID]]))
+			rules.write("{}\t{}\n".format(rule[0], rule[1]))
+		rules.write("\n\n")
+	rules.close()	
 
 	# store rules for each interations
 	i = 0
@@ -54,30 +75,28 @@ def make_rules(orig_data, repaired_files, output, beam_width, min_covered_exampl
 		# filter rule list for repaired data
 		learner_repaired.filter = True
 		learner_repaired.rule_filter = classifier_orig.rule_list
+		learner_repaired.tag = tag
+
+		learner_repaired.rule_finder.weighted_rules = True
+		learner_repaired.rule_finder.scores = scores
 
 		# Produce rules for Repaired Data
 		classifier_repaired = learner_repaired(repaired_data)
 
-		for rule in classifier_repaired.rule_list:
-			for og_rule in og_rulelist:
-				if rule.equals(og_rule):
-					rule.ID = og_rule.ID
-			rule_counts[rule.ID] = i
-		print("Rules produced for {} and rule scores updated".format(repaired_file))
+		if False:
+			for rule in classifier_repaired.rule_list:
+				for og_rule in og_rulelist:
+					#if rule.equals(og_rule):
+					#	rule.ID = og_rule.ID
+					if rule.equals(og_rule):
+						rule.ID = og_rule.ID
+				rule_counts[rule.ID] = i
+			print("Rules produced for {} and rule scores updated".format(repaired_file))
 
 		# Save rules from this iteration
 		rules_produced[i] = classifier_repaired.rule_list
 		i += 1
 
-	
-		#Create rules file from repaired data
-		#with open("Rules/"+output+"-repaired", 'a+') as rules:
-		#	rules.write("******** {} ********\n".format(repaired_file))
-		#	rules.write("Rules\n")
-		#	for rule in classifier_repaired.rule_list:
-		#		rules.write("{}\n".format(rule))
-		#	rules.write("\n\n")
-		#	rules.close()
 	print("Writing rules to file")
 	# write rules to file
 	with open("Rules/"+output+"-repaired", 'a+') as rules:
@@ -85,12 +104,17 @@ def make_rules(orig_data, repaired_files, output, beam_width, min_covered_exampl
 		for j, repaired_file in enumerate(repaired_files):
 			rules.write("******** {} ********\n".format(repaired_file))
 			rules.write("Rules\tScore\n")
+			rl = []
 			for rule in rules_produced[j]:
-				rules.write("{}\t{}\n".format(rule, [rule_counts[rule.ID]]))
+				rl.append((str(rule), rule.score))
+			rl.sort(key=lambda x: x[0])
+			for rule in rl:
+				#rules.write("{}\t{}\n".format(rule, [rule_counts[rule.ID]]))
+				rules.write("{}\t{}\n".format(rule[0], rule[1]))
+
 			rules.write("\n\n")
 	rules.close()	
 
-	
 	# Update log file
 	t = datetime.datetime.now()
 	log = open("log.txt", 'a')
@@ -101,27 +125,78 @@ def make_rules(orig_data, repaired_files, output, beam_width, min_covered_exampl
 	# Open files
 	os.system('xdg-open Rules/'+output+"-repaired")
 
+def make_rules(file, output, beam_width, min_covered_examples, max_rule_length):
+	print("Learning rules from file")
+	# format data for classification
+	data = Orange.data.Table.from_file(file)
+	# set the learner
+	learner = Orange.classification.rules.CN2SDLearner()
+	# set the number of solution steams considered at one time
+	learner.rule_finder.search_algorithm.beam_width = beam_width
+	# continuous value space is constrained to reduce computation time
+	learner.rule_finder.search_strategy.constrain_continuous = True
+	# set the minimum number of examples a found rule must cover to be considered
+	learner.rule_finder.general_validator.min_covered_examples = min_covered_examples
+	# set the maximum number of selectors (conditions) found rules may combine
+	learner.rule_finder.general_validator.max_rule_length = max_rule_length
+	# produce rules from unrepaired data
+	classifier = learner(data)
+
+	print("Writing rules to file")
+	# write rules to file
+	with open("Rules/"+output, 'a+') as rules:
+		rules.write("******** {} ********\n".format(file))
+		rules.write("Rules\n")
+		# Create rules file from repaired data
+		for rule in classifier.rule_list:
+			rules.write("{}\n".format(rule))
+	rules.close()	
+
+	# Update log file
+	t = datetime.datetime.now()
+	log = open("log.txt", 'a')
+	log.write("{}-{}-{} {}:{}:{}\n".format(t.year,t.month,t.day,t.hour,t.minute,t.second))
+	log.write("Data: {}\tBeam Width: {}\tmin_covered_examples: {}\t max_rule_length: {}\n".format(data, beam_width, min_covered_examples, max_rule_length))
+	log.close()
+
+	# Open files
+	os.system('xdg-open Rules/'+output)
+
 
 if __name__ == "__main__":
-	if len(sys.argv) != 6:
-		print("make_rules.py: <original data> <repaired data> <beam width> <minimum covered examples> <maximum rule length>\n")
+	if len(sys.argv) == 1:
+		print("make_rules.py: <original data> <repaired data> <beam width> <minimum covered examples> <maximum rule length> <summary file>\n")
 
 		if len(sys.argv) > 1 and sys.argv[1] == '-dir':
 			for dataset in os.listdir(path_to_datatables):
 				print(dataset)
 
 		sys.exit()
-
+	
 	orig_data = sys.argv[1]
 	repaired_data = sys.argv[2]
 	beam_width = int(sys.argv[3])
 	min_covered_examples = int(sys.argv[4])
 	max_rule_length = int(sys.argv[5])
 
-	repaired_files = sorted([repaired_data+file for file in os.listdir(repaired_data)])
+	# convert score
+	summary = sys.argv[6]
+	tag = sys.argv[7]
+	f = open(summary, 'r')
+	scores_data = ast.literal_eval(f.readline())
+	scores = {}
+	for element in scores_data:
+		scores[element[0]] = element[1]
+		scores[element[0]+tag] = 0
+
+	if os.path.isdir(repaired_data) == True:
+		repaired_files = sorted([repaired_data+"/"+file for file in os.listdir(repaired_data)])
+	else:
+		repaired_files = [repaired_data]
 
 	# Add timestamp identifier to output file
 	t = datetime.datetime.now()
 	output = "{}-{}.{}H{}".format(t.month,t.day,t.hour,t.minute)
 
-	make_rules(orig_data, repaired_files, output, beam_width, min_covered_examples, max_rule_length)
+	make_repaired_rules(orig_data, repaired_files, output, beam_width, min_covered_examples, max_rule_length, scores, tag)
+	
